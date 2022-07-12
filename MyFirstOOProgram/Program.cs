7bb573d440fa6dbdc4905adbd1fed00962719aa0
krolevska <krolevska@gmail.com>
2022-07-12 20:31:29 +0300

    First object&class program

--- MyFirstOOProgram/Program.cs
+++ MyFirstOOProgram/Program.cs
@@ -2,27 +2,42 @@
 
 namespace MyFirstOOProgram
 {
-    class MyClass
+    //In your project, create two classes named MyClassA and MyClassB.
+    //In each of the classes, create one string field and one method that displays the value of the field on the console screen.
+
+    class MyClassA
     {
-        public int field;
+        public string field;
 
         public void Method()
         {
             Console.WriteLine(field);
         }
     }
+    
+    class MyClassB
+    {
+        public string field;
+
+        public void Method()
+        {
+            Console.WriteLine(field);
+        }
+
+    }
+    
+    //In the Main method, create instances of the MyClassA and MyClassB classes,
+    //assign values to the instance fields, and call the methods.
+    
     class Program
     {
         static void Main()
         {
-            MyClass a = new MyClass();
-            MyClass b = new MyClass();
+            MyClassA a = new MyClassA();
+            MyClassB b = new MyClassB();
 
-            a.field = 1;
-            b.field = 2;
-
-            Console.WriteLine(a.field);
-            Console.WriteLine(b.field);
+            a.field = "love";
+            b.field = "sadness";
 
             a.Method();
             b.Method();
@@ -31,7 +46,3 @@ namespace MyFirstOOProgram
         }
     }
 }
-//In your project, create two classes named MyClassA and MyClassB.
-//In each of the classes, create one string field and one method that displays the value of the field on the console screen.
-//In the Main method, create instances of the MyClassA and MyClassB classes,
-//assign values to the instance fields, and call the methods.
