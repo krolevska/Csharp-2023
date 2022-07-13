cc1b72b5f6df00b39b5d560869e7680eb9e11b12
krolevska <krolevska@gmail.com>
2022-07-13 18:39:55 +0300

    Classes and Namespaces

---
+++ MyConsoleApplication_cross-platform/MyConsoleApplication/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using MyPackage;
+
+namespace MyConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyPackage.Console instance = new MyPackage.Console();
+            System.Console.WriteLine("... some results ...");
+            double sqrt = Math.Sqrt(16);
+        }
+    }
+}
