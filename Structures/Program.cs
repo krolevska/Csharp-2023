fb0ba7e62505db2b6fbe2d0ef6cc9361a5a3cf90
krolevska <krolevska@gmail.com>
2022-08-09 21:53:56 +0300

    Structures

---
+++ Structures/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structures1
+{
+    class Program
+    {
+        static void Main()
+        {
+            string model = Console.ReadLine();
+            string producer = Console.ReadLine();
+            decimal price = Convert.ToDecimal(Console.ReadLine());
+            Notebook notebook = new Notebook(model,producer,price);
+            notebook.Show();
+        }
+    }
+
+    struct Notebook
+    {
+        string model;
+        string producer;
+        decimal price;
+
+        public Notebook(string model, string producer, decimal price)
+        {
+            this.model = model;
+            this.producer = producer;
+            this.price = price;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine($"Model — {model}, producer — {producer}, price — {price}");
+        }
+    }
+}
