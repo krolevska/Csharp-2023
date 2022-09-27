0a4d934139a883e6748271d5cdcca0e0444efaf9
krolevska <krolevska@gmail.com>
2022-09-27 19:54:35 +0300

    Generics

---
+++ Generics3/MyDictionary.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics3
+{
+    class MyDictionary<TKey, TValue>
+    {
+        private readonly TKey[] key;
+        private readonly TValue[] value;
+        private readonly int length;
+
+        public int Length
+        {
+            get { return length; }
+        }
+
+        public MyDictionary(int n)
+        {
+            key = new TKey[n];
+            value = new TValue[n];
+            length = n;
+        }
+
+        public string this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index < key.Length)
+                    return key[index] + " - " + value[index];
+                return "Attemp to reach out of array. ";
+            }
+        }
+
+        public void Add(int i, TKey k, TValue l)
+        {
+            key[i] = k;
+            value[i] = l;
+        }
+    }
+}
---
+++ Generics3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics3
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("How big is the dictionary? ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            var dictionary = new MyDictionary<string, string>(n);
+
+            for (int i = 0; i < n; i++)
+            {
+                dictionary.Add(i, "table", "tavolo");
+            }
+
+            Console.WriteLine(new String('-', 20));
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(dictionary[i]);
+            }
+            Console.WriteLine(dictionary[1]);
+            Console.WriteLine(dictionary.Length);
+        }
+    }
+}
