dfc5273d1d01c3f4602d25bbc725fe011dd552e6
katerynakrolevska <122815834+katerynakrolevska@users.noreply.github.com>
2023-05-26 11:15:14 +0300

    Cleaning

--- 002_Classes/Classes1/Properties/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyProduct("C# Essential")]
-[assembly: AssemblyCompany("CyberBionic Systematics")]
-[assembly: AssemblyCopyright("Copyright © CyberBionic Systematics 2015")]
